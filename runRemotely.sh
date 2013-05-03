d0c12d1f313a5d339bb6b495bcb535e9219988bb
Kai <kpeng234@gmail.com>
2013-05-02 19:16:30 -0700

    Again

--- runRemotely.sh
+++ runRemotely.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ssh into machine
-
+ssh 
 
 # pull from git
 
@@ -15,7 +15,7 @@ echo "Running IBController: $CLASSPATH/IBController/IBController.jar"
 
 cd ibGateway/IBJts
 
-java -cp "jts.jar:hsqldb.jar:jcommon-1.0.12.jar:jhall.jar:other.jar:rss.jar:$CLASSPATH/IBController/IBController.jar" "-Dsun.java2d.noddraw=true -Xmx512M -XX:MaxPermSize=128M" ibcontroller.IBGatewayController "$CLASSPATH/IBController/IBcontroller.ini" $1 $2
+java -cp "jts.jar:hsqldb.jar:jcommon-1.0.12.jar:jhall.jar:other.jar:rss.jar:$CLASSPATH/IBController/IBController.jar" "-Dsun.java2d.noddraw=true -Xmx512M -XX:MaxPermSize=128M" ibcontroller.IBGatewayController "$CLASSPATH/IBController/IBController.ini" $1 $2
 
 
 # start AtomicTrader
